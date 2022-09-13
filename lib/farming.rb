8325d17ff54225b92262cf20e3d64cfeb534314c
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-09-13 21:39:23 +0200

    created crop class(superclass)

--- lib/farming.rb
+++ lib/farming.rb
@@ -1,4 +1,5 @@
 require_relative "corn"
+require_relative "rice"
 
 puts "\n\n Day One: Corn"
 corn = Corn.new
@@ -6,3 +7,12 @@ corn.water!
 puts "The corn crop produced #{corn.grains} grains"
 corn_status = corn.ripe? ? "ripe" : "not ripe"
 puts "The corn is #{corn_status}"
+
+
+puts "\n\n Day Two: Rice"
+rice = Rice.new
+rice.water!
+rice.transplant!
+puts "The rice crop produced #{rice.grains} grains"
+rice_status = rice.ripe? ? "ripe" : "not ripe"
+puts "The corn is #{rice_status}"
